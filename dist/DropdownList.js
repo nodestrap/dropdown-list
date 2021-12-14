6dfdbdfbae2b2da3d2ec4bc6b977859a07d9b739
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-12-15 06:17:22 +0700

    tweaked up

--- dist/DropdownList.js
+++ dist/DropdownList.js
@@ -50,7 +50,7 @@ export const useDropdownListElementSheet = createUseSheet(() => [
             ]),
         ]),
     ]),
-]);
+], /*sheetId :*/ 'ib5nas167b'); // an unique salt for SSR support, ensures the server-side & client-side have the same generated class names
 // configs:
 export const [cssProps, cssDecls, cssVals, cssConfig] = createCssConfig(() => {
     return {
